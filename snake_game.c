ea441b15426dcaaf3395aec012e09e34ad538665
brendenfernandes <bfernandes139@gmail.com>
2025-11-19 05:57:24 +0000

    Initialized the ncurses window properly, drew out the bordered play field for the snake, showed snake head at start position, responds to keyoad input in real-time, snake can no move, and exits on pressing 'q'.

--- snake_game.c
+++ snake_game.c
@@ -6,17 +6,41 @@ int main()
     int x = 10; //starting positone of the snake head on x
     int y = 10; //starting positone of the snake head on y
     int ch; //stores the keyboard input
+    int max_x;  //max terminal width
+    int max_y;  //max terminal height
     
 
     initscr();  //initialized the screen, starts ncurses
     noecho();   //stops typed characters from being shown on screen
+    cbreak();
+    keypad(stdscr, TRUE);
+
+    getmaxyx(stdscr, max_y, max_x); //gets the height and width of the window
 
     //while loop for the main game
     while(1)
     {
         clear();    //fresh state of the game
 
-        move(0,0);  //cursor starts at the top left of the frame
+        for(int i = 0; i<max_x; i++)
+        {
+            move(0,i);
+            addch('#');
+
+            move(max_y - 1,i);
+            addch('#');
+        }
+
+        for(int i = 0; i<max_y; i++)
+        {
+            move(i,0);
+            addch('#');
+
+            move(i, max_x -1);
+            addch('#');
+        }
+
+        move(0,2);  //cursor starts at the top left of the frame
         addstr("Snake Game (WASD to move, q to quit)"); //displays game name and instructions
 
         move(y,x);  //cursor moves to place the head of the snake position
@@ -56,7 +80,27 @@ int main()
             x++;
         }
 
-        endwin();   //ends the ncurses and returns to normal terminal
-        return 0;   //returns 0 and ends program
+        //keeps snake within the borders
+        if(x<=1)
+        {
+            x=1;
+        }
+        if(x>=max_x -2)
+        {
+            x=max_x -2;
+        }
+        if(y<=1)
+        {
+            y=1;
+        }
+        if(y>=max_y -2)
+        {
+            y=max_y -2;
+        }
+
+        
     }
+    
+    endwin();   //ends the ncurses and returns to normal terminal
+    return 0;   //returns 0 and ends program
 }
