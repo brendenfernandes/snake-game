197ac859ef84ae1c7aac95047c230549b68e97fa
brendenfernandes <bfernandes139@gmail.com>
2025-11-20 04:58:13 +0000

    Added food spawing within the play field. Food can be eaten by the snake and reappears after being eaten.

--- snake_game.c
+++ snake_game.c
@@ -1,5 +1,7 @@
 #include <ncurses.h>
 #include <unistd.h>
+#include <time.h>
+#include <stdlib.h>
 
 int main()
 {
@@ -8,12 +10,16 @@ int main()
     int ch; //stores the keyboard input
     int max_x;  //max terminal width
     int max_y;  //max terminal height
+    int food_x; //food position x axis
+    int food_y; //food position y axis
+    int food_exists = 0;    //if food is on screen
     
+    srand(time(NULL));
 
     initscr();  //initialized the screen, starts ncurses
     noecho();   //stops typed characters from being shown on screen
-    cbreak();
-    keypad(stdscr, TRUE);
+    cbreak();   //gets input
+    keypad(stdscr, TRUE);   //allows special keys
 
     getmaxyx(stdscr, max_y, max_x); //gets the height and width of the window
 
@@ -22,6 +28,7 @@ int main()
     {
         clear();    //fresh state of the game
 
+        //draws the top and bottom border frames of play area
         for(int i = 0; i<max_x; i++)
         {
             move(0,i);
@@ -31,6 +38,7 @@ int main()
             addch('#');
         }
 
+        //draws the left and right border frames of play area
         for(int i = 0; i<max_y; i++)
         {
             move(i,0);
@@ -42,6 +50,18 @@ int main()
 
         move(0,2);  //cursor starts at the top left of the frame
         addstr("Snake Game (WASD to move, q to quit)"); //displays game name and instructions
+
+
+        //spwans food into within play field if food is not existing
+        if(!food_exists)
+        {
+            food_x = (rand() % (max_x - 2)) + 1;
+            food_y = (rand() % (max_y - 2)) + 1;
+            food_exists = 1;
+        }
+
+        move(food_y, food_x);   //cursor moves to place food
+        addch('*'); //draws the food
 
         move(y,x);  //cursor moves to place the head of the snake position
         addstr("@");    //draws the head of the snake
@@ -96,6 +116,12 @@ int main()
         if(y>=max_y -2)
         {
             y=max_y -2;
+        }
+
+        //checks if snake has eaten food
+        if(x == food_x && y == food_y)
+        {
+            food_exists = 0;    //causes food to spawn in next loop
         }
 
         
